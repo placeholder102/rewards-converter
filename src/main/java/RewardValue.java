567b459b2bf957fbcf56481ee3c81741091f2d72
Roman Ramirez <roman.mlg.115@gmail.com>
2024-08-14 13:58:44 -0500

    changes03

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    private final double cashValue;
+    private final double milesValue;
+
+    //cash value
+    public RewardValue(double CashValue) {
+        this.cashValue = CashValue;
+        this.milesValue = convertToCash((int) cashValue);
+    }
+    //miles value
+    public RewardValue(int miles) {
+        this.milesValue = miles;
+        this.cashValue = convertToMiles(miles) ;
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / 0.0035);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * 0.0035;
+    }
+
+    //get the cash value of RewardValue
+    public double getCashValue() {
+        return cashValue;
+    }
+    // miles value rate
+    public double getMilesValue() {
+        return milesValue;
+    }
+    //commit & push
+
 }
+
+
